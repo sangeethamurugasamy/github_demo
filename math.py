12fc3806cdc46d611b6d1c44dba17212f6100976
sangeethamurugasamy <52697526+sangeethamurugasamy@users.noreply.github.com>
2021-07-08 01:58:56 -0700

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+# add implementation
+def add(x,y):
+ return x+y
+ # sub implementation
+def subtract(x,y): 
+ pass 
+ # mul implementation
+ #mul
+def multiply(x,y):
+ pass 
+  # div implementation
+  #div
+def divide(x,y):
+ pass
